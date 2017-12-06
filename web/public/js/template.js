543a5f6c4591307385c10190ddec48425f373d8a
typefo <typefo@qq.com>
2017-12-06 17:54:33 +0800

    updated some files

--- web/public/js/template.js
+++ web/public/js/template.js
@@ -20,7 +20,7 @@ function show() {
     layer.open({
         type: 1,
         title: '新建签名模板',
-        area: ['550px', '360px'],
+        area: ['650px', '350px'],
         content: template
     });
 }
@@ -43,9 +43,9 @@ function formSubmit() {
     xhr.send(data);
 }
 
-function formChange() {
+function formChange(id) {
     var method = "POST";
-    var url = '/template/update?id=' + $("input[name=id]").val();
+    var url = '/template/update?id=' + id;
     var form = document.getElementById("form");
     var data = new FormData(form);
     var xhr = new XMLHttpRequest();
@@ -61,26 +61,26 @@ function formChange() {
     xhr.send(data);
 }
 
-function editTemplate(id) {
-    var template = document.getElementById("edit-page").innerHTML;
-
-    layer.open({
-        type: 1,
-        title: '签名模板',
-        area: ['550px', '360px'],
-        content: template
-    });
-
+function changeTemplate(id) {
+    var method = "GET";
     var url = '/api/template?id=' + id;
-    $.get(url, function(resp, stat){
-        if (stat == 'success') {
-            var el = resp.data;
-            $("input[name=id]").val(el.id);
-            $("input[name=rexp]").val(el.rexp);
-            $("input[name=name]").val(el.name);
-            $("textarea[name=contents]").val(el.contents);
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+            var source = document.getElementById("edit-page").innerHTML;
+            var template = Handlebars.compile(source);
+            var contents = template(xhr.response.data);
+            layer.open({
+                type: 1,
+                title: '编辑签名模板',
+                area: ['650px', '350px'],
+                content: contents
+            });
         }
-    });
+    }
+    xhr.open(method, url, true);
+    xhr.send();
 }
 
 function deleteTemplate(id) {
