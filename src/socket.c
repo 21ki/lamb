1929265d520224460ca0e661e3327a9d1c376f75
typefo <typefo@qq.com>
2017-12-21 18:08:06 +0800

    updated some files

--- src/socket.c
+++ src/socket.c
@@ -11,6 +11,7 @@
 #include <nanomsg/pair.h>
 #include <nanomsg/reqrep.h>
 #include "socket.h"
+#include "command.h"
 
 int lamb_nn_request(int *sock, const char *host, int port, int timeout) {
     int fd;
@@ -39,46 +40,84 @@ int lamb_nn_request(int *sock, const char *host, int port, int timeout) {
 int lamb_nn_connect(int *sock, lamb_nn_option *opt, const char *host, int port, int protocol, int timeout) {
     int err;
     int fd, rc;
-    char *buf;
+    void *buf;
     char url[128];
-    lamb_req_t req;
-    lamb_rep_t *rep;
     int timeo;
-    
+        
     err = lamb_nn_request(&fd, host, port, timeout);
     if (err || fd < 0) {
         return -1;
     }
 
+    int *method;
+    void buff[8192];
+
+    method = buff;
+    *method = LAMB_REQUEST;
+
+    unsigned int len;
+    Request req = LAMB_REQUEST_INIT;
+    req.id = opt->id;
+    req.type = opt->type;
+    req.addr = opt->addr;
+
+    len = lamb_request_get_packed_size(&req) + sizeof(int);
+
+    lamb_request_pack(&req, buff + sizeof(int));
+
+    /* old code */
+    /* 
     memset(&req, 0, sizeof(req));
     req.id = opt->id;
     req.type = opt->type;
     memcpy(req.addr, opt->addr, 16);
+    */
 
-    rc = nn_send(fd, (char *)&req, sizeof(req), 0);
+    rc = nn_send(fd, buff, len, 0);
+
     if (rc < 0) {
         nn_close(fd);
         return 1;
     }
 
     rc = nn_recv(fd, &buf, NN_MSG, 0);
-    if (rc < 0) {
+    if (rc < 4) {
         nn_close(fd);
         return 2;
     }
-    
+
+    /* 
     if (rc != sizeof(lamb_rep_t)) {
         nn_close(fd);
         nn_freemsg(buf);
         return 3;
     }
+ */
 
     nn_close(fd);
-    rep = (lamb_rep_t *)buf;
 
+    // rep = (lamb_rep_t *)buf;
+
+    method = (int *)buf;
+
+    /* Check method */
+    if (method != LAMB_RESPONSE) {
+        nn_freemsg(buf);
+        return 3;
+    }
+
+    Response *rep;
+    rep = lamb_response_unpack(NULL, rc - sizeof(int), buf + sizeof(int));
+
+    if (!rep) {
+        nn_freemsg(buf);
+        return 4;
+    }
+    
     fd = nn_socket(AF_SP, protocol);
     if (fd < 0) {
         nn_freemsg(buf);
+        lamb_response_free_unpacked(rep, NULL);
         return 4;
     }
 
@@ -86,6 +125,9 @@ int lamb_nn_connect(int *sock, lamb_nn_option *opt, const char *host, int port, 
     nn_setsockopt(fd, NN_SOL_SOCKET, NN_SNDTIMEO, &timeo, sizeof(timeo));
 
     sprintf(url, "tcp://%s:%d", rep->addr, rep->port);
+
+    /* free response packet */
+    lamb_response_free_unpacked(rep, NULL);
 
     if (nn_connect(fd, url) < 0) {
         nn_freemsg(buf);
