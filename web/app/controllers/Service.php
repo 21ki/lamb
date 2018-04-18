5955d2b705505692cbf8b5f63faf3368c2488813
typefo <typefo@qq.com>
2018-04-18 18:04:35 +0800

    updated some files

---
+++ web/app/controllers/Service.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * The Service Controller
+ * Link http://github.com/typefo/lamb
+ * Copyright (C) typefo <typefo@qq.com>
+ */
+
+class ServiceController extends Yaf\Controller_Abstract {
+    public function coreAction() {
+        return true;
+    }
+
+    public function serverAction() {
+        return true;
+    }
+
+    public function gatewayAction() {
+        return true;
+    }
+
+    public function mtAction() {
+        return true;
+    }
+
+    public function moAction() {
+        return true;
+    }
+}
+
+
